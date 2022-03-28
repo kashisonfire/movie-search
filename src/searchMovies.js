08fa2a4a7a692ccb086dfad12fb729b89e6297e4
Akash Patel <akash.patel.mae@gmail.com>
2022-03-28 07:15:44 -0700

    completed

---
+++ src/searchMovies.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import MovieCard from "./components/movieCard";
+
+export default function SearchMovies(){
+
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=0514d0164acc6b02cd0e43545f85462b&language=en-US&query=${query}&page=1&include_adult=false`
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data.results);
+            setMovies(data.results);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input className="input" type="text" name="query" 
+                    placeholder="i.e. Jurassic Park"
+                    value={query} onChange={(e) => setQuery(e.target.value)}/>
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => 
+                    <MovieCard movie={movie} key={movie.id} />
+                )}
+            </div>    
+        </>
+    )
+}
